807edb3634db90dc6c82fa1f850ba0ae2d505087
John James Gracia Ermitaño <John James G. Ermitano>
2017-10-18 23:23:18 +0800

    initial storage commit

--- src/app/storage/storage-routing.module.ts
+++ src/app/storage/storage-routing.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { StorageComponent } from './storage.component';
+import { UploadComponent } from './upload/upload.component';
+import { DownloadComponent } from './download/download.component';
+import { RemoveComponent } from './remove/remove.component';
 
 const routes: Routes = [
-  { path: '', component: StorageComponent }
+  { path: '', component: StorageComponent, children: [
+    { path: 'upload', component: UploadComponent },
+    { path: 'download', component: DownloadComponent },
+    { path: 'remove', component: RemoveComponent }
+  ] }
 ];
 
 
