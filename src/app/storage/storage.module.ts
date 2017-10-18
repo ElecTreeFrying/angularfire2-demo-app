793d0fd7a97f49e6a7c93e820a0536e3cb1bff02
John James Gracia Ermitaño <John James G. Ermitano>
2017-10-18 23:17:34 +0800

    Added upload, download and remove declarations and providers

--- src/app/storage/storage.module.ts
+++ src/app/storage/storage.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { StorageRoutingModule } from './storage-routing.module';
+
 import { StorageComponent } from './storage.component';
+import { OptionsComponent } from './options/options.component';
+import { UploadComponent } from './upload/upload.component';
+import { DownloadComponent } from './download/download.component';
+import { RemoveComponent } from './remove/remove.component';
+
+import { DownloadService } from './download/download.service';
+import { UploadService } from './upload/upload.service';
+import { RemoveService } from './remove/remove.service';
+
 
 @NgModule({
   imports: [
     CommonModule,
     StorageRoutingModule
   ],
-  declarations: [StorageComponent]
+  declarations: [
+    StorageComponent,
+    OptionsComponent,
+    UploadComponent,
+    DownloadComponent,
+    RemoveComponent
+  ],
+  providers: [
+    DownloadService,
+    UploadService,
+    RemoveService
+  ]
 })
 export class StorageModule { }
