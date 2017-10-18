4ddadc42d1263c92bc74e809a7942a4f1968aef0
John James Gracia Ermitaño <John James G. Ermitano>
2017-10-19 03:02:26 +0800

    initial authentication commit

---
+++ src/app/authentication/options/options.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-options',
+  templateUrl: './options.component.html',
+  styleUrls: ['./options.component.scss']
+})
+export class OptionsComponent implements OnInit {
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+  }
+
+  onNavigate(options: string): void {
+    this.router.navigate([options], { relativeTo: this.route });
+  }
+
+
+}
