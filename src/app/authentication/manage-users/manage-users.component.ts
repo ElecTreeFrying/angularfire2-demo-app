86dde5a77059c92d5614d3126800d5a0d1f5acfa
John James Gracia Ermitaño <John James G. Ermitano>
2017-10-19 03:02:12 +0800

    initial manage-users commit

---
+++ src/app/authentication/manage-users/manage-users.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ManageUsersService } from './manage-users.service';
+
+
+@Component({
+  selector: 'app-manage-users',
+  templateUrl: './manage-users.component.html',
+  styleUrls: ['./manage-users.component.scss']
+})
+export class ManageUsersComponent implements OnInit {
+
+  constructor(private manageUsersService: ManageUsersService) { }
+
+  ngOnInit() {
+  }
+
+}
