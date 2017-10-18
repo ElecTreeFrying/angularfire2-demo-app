60b15f39bac3a378bc5e0ae4be38491ff3e151ad
John James Gracia Ermitaño <John James G. Ermitano>
2017-10-19 02:58:02 +0800

    removed hosting route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HostingComponent } from './hosting/hosting.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
@@ -8,7 +7,6 @@ const routes: Routes = [
   { path: 'rtdb', loadChildren: './realtime-database/realtime-database.module#RealtimeDatabaseModule' },
   { path: 'firestore', loadChildren: './firestore/firestore.module#FirestoreModule' },
   { path: 'storage', loadChildren: './storage/storage.module#StorageModule' },
-  { path: 'hosting', component: HostingComponent }
 ];
 
 
