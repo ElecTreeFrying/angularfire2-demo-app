0326a53e1317b1b0d86b11acdde33cc8638930d1
ElecTreeFrying <johnjamesermitano@yahoo.com>
2018-07-18 18:44:39 +0800

    add: firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  firebaseConfig: {}
+  firebaseConfig: {
+    apiKey: "AIzaSyC8Gf4GCVu_notQr4-wjH9UTRb9VBSmJUk",
+    authDomain: "angularfire2-demo-app.firebaseapp.com",
+    databaseURL: "https://angularfire2-demo-app.firebaseio.com",
+    projectId: "angularfire2-demo-app",
+    storageBucket: "angularfire2-demo-app.appspot.com",
+    messagingSenderId: "469879317588"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,14 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {}
+  firebaseConfig: {
+    apiKey: "AIzaSyC8Gf4GCVu_notQr4-wjH9UTRb9VBSmJUk",
+    authDomain: "angularfire2-demo-app.firebaseapp.com",
+    databaseURL: "https://angularfire2-demo-app.firebaseio.com",
+    projectId: "angularfire2-demo-app",
+    storageBucket: "angularfire2-demo-app.appspot.com",
+    messagingSenderId: "469879317588"
+  }
 };
 
 /*
